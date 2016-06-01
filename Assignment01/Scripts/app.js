21e907c796838550832826ed59c98b3c03f7cd16
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-06-01 13:59:19 -0400

    Added CSS and Scripts

---
+++ Assignment01/Scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// setup your IIFE (Immediately Invoked Function Expression)
+(function () {
+
+	console.log("App Started...");
+
+	var about;
+	about = document.getElementById("about");
+	document.getElementById("about").innerHTML = "I Create & Design the Websites by the process of collecting ideas,and implementing them,guided by certain principles for a specific purpose.Web design is a similar process of creation,with the intention to create the Websites which usually includes a concerted focus on performance, aesthetics, user experience and interaction, social media engagement and web presence optimization for presenting the content on electronic web pages."
+	
+})();
